fa31a2795c57e82f6d8f012f8c1571916a1690f7
AightBet7 <rishaangoenka@gmail.com>
2026-08-08 11:50:25 +0800

    Change Supabase publishable key
    Updated Supabase key to a new publishable key.

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://vmrnldfrhthohmnapznc.supabase.co";
 
-const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_KEY = sb_publishable_o_82vTY4KdoJUHNj8PDkBA_HkkX08VK;
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
