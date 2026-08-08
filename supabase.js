1f7816b4727fc7331427790c8246cb1a2c2e8101
AightBet7 <rishaangoenka@gmail.com>
2026-08-08 15:58:33 +0800

    Fix SUPABASE_KEY string formatting

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://vmrnldfrhthohmnapznc.supabase.co";
 
-const SUPABASE_KEY = sb_publishable_o_82vTY4KdoJUHNj8PDkBA_HkkX08VK;
+const SUPABASE_KEY = "sb_publishable_o_82vTY4KdoJUHNj8PDkBA_HkkX08VK";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
